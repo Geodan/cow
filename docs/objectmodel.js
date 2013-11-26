0fc22af82a62e35ac566121d59e327b14affa4af
tomvantilburg <tom.van.tilburg@gmail.com>
2013-11-26 17:03:45 +0100

    Working on objectmodel

--- docs/objectmodel.js
+++ docs/objectmodel.js
@@ -1,14 +1,44 @@
-{_synstore:
+_synstore: //Synstore keeps track of records 
+{
+    _pouchdb: {}, //pouchdb object
+    _
+    
+    
 }
+group: 
+{
+    _users: [ID], //Possibly we can forget about keeping track of users in the group
+    _groups: [ID], //
+    getUsers: function(){},
+    addUser: function(ID){},
+    removeUser: function(ID){},
+    
+}
+
 project:
 {
-    _groupstore:{}, //Inherits from syncStore
+    _groupStore:{
+        _records: [group],
+        getGroups: function(){},
+        getGroup: function(ID){},
+        addGroup: function(config){},
+        updateGroup: function(config){},
+        removeGroup: function(ID){}
+    }, //Inherits from syncStore
+    
     _itemstore:{}, //Inherits from syncStore
+    
     _members:[]
 }
 
 {
     core: {
+        /*MYSPECS*/
+        _mySpecs: { //Contain
+        
+        
+        
+        /*PROJECTS*/
         _projectStore:  { //Inherits from syncStore
             _records: [project],
             getProjects: function(){}, //returns all projects
@@ -19,14 +49,17 @@ project:
         },
         projectStore: function(), //returns the _projectstore object
         
-        _peers:     [
-            
-        ],
+        /*PEERS*/
+        _peers:     [peer],
         getPeers: function(){}, //returns all peer objects
         getPeer: function(ID){}, //return 1 peer
         addPeer: function(config){},
         updatePeer: function(config){},
+        removePeer: function(ID){}, //remove peer from _peers
+        getPeerExtents: function(){}, //returns featurecollection of peerextents
+        getPeerPositions: function(){},//returns featurecollection of peerpositions
         
+        /*USERS*/
         _userStore:     { //Inherits from syncStore
             _records: [user],
             getUsers: function(){},
@@ -37,6 +70,7 @@ project:
         },
         userStore: function(){}, //returns the _userStore object
         
+        /*WEBSOCKET*/
         _websocket: {
             _connection: {
                 //socket connection object
@@ -44,7 +78,7 @@ project:
             connect: function(URL){},
             disconnect: function(){},
             sendData: function(data, action, target){},
-            _onMessage: function(){},
+            _onMessage: function(d){},
             _onClose: function(e){},
             _onConnect: function(d){},
             _onError: function(e){},
