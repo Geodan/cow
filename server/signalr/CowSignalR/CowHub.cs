78b4a9843c501387dcc469e8ab801fc873631a38
bertt <btemme@hotmail.com>
2014-03-04 14:57:34 +0100

    initial singalr server

---
+++ server/signalr/CowSignalR/CowHub.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+
+namespace CowSignalR
+{
+    public class CowHub : Hub
+    {
+        public override Task OnConnected()
+        {
+            Clients.Others.userConnected(Context.ConnectionId);
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected()
+        {
+            Clients.Others.userDisconnected(Context.ConnectionId);
+            return base.OnDisconnected();
+        }
+
+        public override Task OnReconnected()
+        {
+            Clients.Others.userReconnected(Context.ConnectionId);
+            return base.OnDisconnected();
+        }
+
+        public void Send(string message)
+        {
+            Clients.All.broadcastMessage(message);
+        }
+    }
+}
---
+++ server/signalr/CowSignalR/Startup.cs
@@ -0,0 +1,17 @@
+﻿using CowSignalR;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace CowSignalR
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR();
+        }
+    }
+}
